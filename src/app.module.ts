a0eed85a2890eb472e130fd07629b4ec8d0a80b6
daviribeiro <luminuszz43@gmail.com>
2023-05-23 16:38:16 -0300

    add helmet rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { NotificationModule } from './modules/notification/notification.module';
 import { WorkModule } from './modules/work/work.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     ScheduleModule.forRoot(),
     CqrsModule,
     WorkModule,
@@ -32,6 +38,6 @@ import { WorkModule } from './modules/work/work.module';
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
