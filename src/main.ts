d633759dfddb49691aeebd20a85d9f25dedae820
daviribeiro <luminuszz43@gmail.com>
2023-05-23 10:47:36 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://okami-front-end-61mg.vercel.app/',
-      'http://localhost:3000',
-    ],
+    origin: '*',
     allowedHeaders: '*',
     methods: '*',
   });
