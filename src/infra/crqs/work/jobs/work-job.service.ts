7c2a6e17bd4270137005d57efde967d066583d66
luminuszz <luminuszz43@gmail.com>
2023-07-07 21:43:36 -0300

    fix(jobs): fix duplicate on stack jobs

--- src/infra/crqs/work/jobs/work-job.service.ts
+++ src/infra/crqs/work/jobs/work-job.service.ts
@@ -1,9 +1,10 @@
-import { Category } from '@domain/work/enterprise/entities/work';
+import { Category, Work } from '@domain/work/enterprise/entities/work';
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Queue } from 'bull';
 import { FetchWorksForScrappingUseCase } from '@domain/work/application/usecases/fetch-works-for-scrapping';
+import { JobId } from '@infra/crqs/work/jobs/JobId';
 
 interface FindSerieEpisodeDTO {
   id: string;
@@ -30,34 +31,54 @@ export class WorkJobsService {
     private readonly fetchWorksForScrappingUseCase: FetchWorksForScrappingUseCase,
   ) {}
 
-  @Cron(CronExpression.EVERY_6_HOURS)
+  private async addFindSerieEpisodeQueue(work: Work) {
+    const jobId = JobId.getJobId(work.id, work.chapter.getChapter());
+
+    const job = await this.findSerieEpisodeQueue.getJob(jobId);
+
+    if (!job) {
+      await this.findSerieEpisodeQueue.add(
+        {
+          id: work.id,
+          url: work.url,
+          episode: work.chapter.getChapter(),
+          name: work.name,
+        },
+        { removeOnComplete: true, jobId },
+      );
+    }
+  }
+
+  private async addFindChapterQueue(work: Work) {
+    const jobId = JobId.getJobId(work.id, work.chapter.getChapter());
+
+    const job = await this.findChapterQueue.getJob(jobId);
+
+    if (!job) {
+      await this.findChapterQueue.add(
+        {
+          id: work.id,
+          url: work.url,
+          cap: work.chapter.getChapter(),
+          name: work.name,
+        },
+        { removeOnComplete: true, jobId },
+      );
+    }
+  }
+
+  @Cron(CronExpression.EVERY_6_HOURS, { name: 'fetch-works-for-scrapping' })
   async triggerQueueFindSerieEpisodeQueue() {
     const { value } = await this.fetchWorksForScrappingUseCase.execute();
 
-    value.works.forEach((work) => {
+    for (const work of value.works) {
       if (work.category === Category.ANIME) {
-        this.findSerieEpisodeQueue.add(
-          {
-            id: work.id,
-            url: work.url,
-            episode: work.chapter.getChapter(),
-            name: work.name,
-          },
-          { removeOnComplete: true },
-        );
+        await this.addFindSerieEpisodeQueue(work);
       }
 
       if (work.category === Category.MANGA) {
-        this.findChapterQueue.add(
-          {
-            id: work.id,
-            url: work.url,
-            cap: work.chapter.getChapter(),
-            name: work.name,
-          },
-          { removeOnComplete: true },
-        );
+        await this.addFindChapterQueue(work);
       }
-    });
+    }
   }
 }
