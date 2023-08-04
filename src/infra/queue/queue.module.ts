54277f3a9d177206d210b2571bfee68fb6c800db
luminuszz <luminuszz43@gmail.com>
2023-08-04 20:11:50 -0300

    ci(aws-ec2):  config and prepare envs and secrets and pipiline for ec2

--- src/infra/queue/queue.module.ts
+++ src/infra/queue/queue.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BullQueueProvider } from '@infra/queue/bull-queue.provider';
 import { QueueProvider } from '@domain/work/application/contracts/queueProvider';
+import loadSecrets from '@infra/utils/getSecretesEnv';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      load: [loadSecrets],
+    }),
+  ],
   providers: [
     {
       provide: QueueProvider,
