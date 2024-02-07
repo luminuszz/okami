053d83bee928b0ede29eca1b968e746344dd9ba6
luminuszz <luminuszz43@gmail.com>
2024-02-07 11:08:51 -0300

    feat(infra): create endpoints for get public key

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { SubscribeUserBrowserNotificationDto } from '../validators/subscribe-user-browser-notification.dto';
 import { CreateBrowserUserNotificationSubscriptionCommand } from '@app/infra/crqs/notification/commands/create-user-notification-subscription.command';
 import { AuthGuard } from '@app/infra/crqs/auth/auth.guard';
 import { UserTokenDto } from '@app/infra/crqs/auth/dto/user-token.dto';
 import { CommandBus } from '@nestjs/cqrs';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('notification')
 export class NotificationController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private configService: ConfigService,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Post('/push/subscribe/browser')
@@ -28,5 +32,13 @@ export class NotificationController {
         userId: user.id,
       }),
     );
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/push/browser/public-key')
+  async getPublicKey() {
+    return {
+      publicKey: this.configService.get('WEB_PUSH_PUBLIC_KEY'),
+    };
   }
 }
