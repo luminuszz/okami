16ebb9eacf79bcbf1c1a4f0864a2f9796c9bdc99
luminuszz <luminuszz43@gmail.com>
2024-06-27 16:57:12 -0300

    feat(Infra): implement emailProvider interface sendConfirmEmail
    Signed-off-by: luminuszz <luminuszz43@gmail.com>

--- src/infra/mail/resend-provider.service.ts
+++ src/infra/mail/resend-provider.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { EnvService } from '../env/env.service';
 import { Resend } from 'resend';
-import { MailProvider, SendResetPasswordEmailDto } from '@domain/auth/application/contracts/mail-provider';
+import {
+  MailProvider,
+  SendConfirmEmailDto,
+  SendResetPasswordEmailDto,
+} from '@domain/auth/application/contracts/mail-provider';
 
 @Injectable()
 export class ResendEmailProviderService implements MailProvider {
@@ -9,6 +13,19 @@ export class ResendEmailProviderService implements MailProvider {
 
   constructor(private readonly env: EnvService) {
     this.resend = new Resend(env.get('RESEND_API_SECRET_KEY'));
+  }
+
+  async sendConfirmEmail({ email, confirmEmailCode }: SendConfirmEmailDto): Promise<void> {
+    await this.resend.emails.send({
+      to: email,
+      subject: "'Okami Platform'",
+      text: `
+        Olá, você se cadastrou na plataforma Okami.
+        use o código abaixo para confirmar seu email
+        ${confirmEmailCode}
+      `,
+      from: 'Okami Platform <okami@okami-mail.daviribeiro.com>',
+    });
   }
 
   async sendResetPasswordEmail({ email, resetPasswordCode }: SendResetPasswordEmailDto): Promise<void> {
