b3c516e241a3af9453795beb5c985cbebcc9b4ea
luminuszz <luminuszz43@gmail.com>
2024-07-29 10:05:04 -0300

    feat(infra): compress and transform images
    Create new step on upload image new images for compres image to 60 quality and transform image in jpg
    
    Signed-off-by: luminuszz <luminuszz43@gmail.com>

---
+++ src/infra/storage/image-transformer.provider.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+
+import sharp from 'sharp';
+
+export interface ImageData {
+  fileName: string;
+  fileData: Buffer | ArrayBuffer;
+}
+
+export interface ImageResponse {
+  fileName: string;
+  fileData: Buffer;
+  fileMimeType: string;
+}
+
+export abstract class ImageTransformerProvider {
+  abstract compressAndTransformImageToJPG(image: ImageData): Promise<ImageResponse>;
+}
+
+@Injectable()
+export class SharpImageTransformerProvider implements ImageTransformerProvider {
+  async compressAndTransformImageToJPG({ fileName, fileData }: ImageData): Promise<ImageResponse> {
+    const fileBuffer = await sharp(fileData)
+      .webp({
+        quality: 60,
+      })
+      .toBuffer();
+
+    return {
+      fileData: fileBuffer,
+      fileMimeType: 'jpg',
+      fileName,
+    };
+  }
+}
