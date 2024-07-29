c9616b27da60ce366a45f8b2f0cf47c67b90fff8
luminuszz <luminuszz43@gmail.com>
2024-07-29 11:15:29 -0300

    chore: add mock for develop
    Signed-off-by: luminuszz <luminuszz43@gmail.com>

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule } from '@nestjs/microservices';
 import { MessageService } from './messaging-service';
+import { ClientsModule } from '@nestjs/microservices';
 import { messageProviderConnectProvider } from '@infra/messaging/messaging-module.config';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const developBlockerMock = {
+  subscribeToResponseOf: () => {},
+  connect: () => {},
+  emit: () => {},
+  send: () => {},
+};
 
 @Module({
   imports: [ClientsModule.registerAsync([messageProviderConnectProvider])],
