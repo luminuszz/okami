8558f3b0c6abcaff90620ff246ae957284ce4f6c
luminuszz <luminuszz43@gmail.com>
2025-01-21 18:08:36 -0300

    fix: kml z-index

--- src/infra/storage/cloudFlare-r2-storage.adapter.ts
+++ src/infra/storage/cloudFlare-r2-storage.adapter.ts
@@ -1,19 +1,19 @@
-import { ListObjectsV2Command, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { ListObjectsV2Command, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import {
+  FileUploadResponse,
   FiletoUpload,
   FiletoUploadWithUrl,
-  FileUploadResponse,
   StorageProvider,
-} from '@domain/work/application/contracts/storageProvider';
-import { ImageTransformerProvider } from '@infra/storage/image-transformer.provider';
-import { Injectable } from '@nestjs/common';
-import { EnvService } from '../env/env.service';
+} from '@domain/work/application/contracts/storageProvider'
+import { ImageTransformerProvider } from '@infra/storage/image-transformer.provider'
+import { Injectable } from '@nestjs/common'
+import { EnvService } from '../env/env.service'
 
 @Injectable()
 export class CloudFlareR2StorageAdapter implements StorageProvider {
-  private readonly s3Client: S3Client;
-  public readonly awsBucket: string;
+  private readonly s3Client: S3Client
+  public readonly awsBucket: string
 
   constructor(
     private readonly env: EnvService,
@@ -26,40 +26,41 @@ export class CloudFlareR2StorageAdapter implements StorageProvider {
         accessKeyId: this.env.get('CLOUD_FLARE_R2_KEY'),
         secretAccessKey: this.env.get('CLOUD_FLARE_R2_SECRET_KEY'),
       },
-    });
+      forcePathStyle: true,
+    })
 
-    this.awsBucket = this.env.get('CLOUD_FLARE_BUCKET');
+    this.awsBucket = this.env.get('CLOUD_FLARE_BUCKET')
   }
 
   async createUploadUrl(fileName: string, fileType: string): Promise<string> {
-    await this.createFolderIfNotExists('work-images');
+    await this.createFolderIfNotExists('work-images')
 
     const puts3Command = new PutObjectCommand({
       Bucket: this.awsBucket,
       Key: `work-images/${fileName}.${fileType}`,
       ContentType: `image/${fileType}`,
-    });
+    })
 
-    const uploadUrl = await getSignedUrl(this.s3Client, puts3Command, { expiresIn: 3600 });
+    const uploadUrl = await getSignedUrl(this.s3Client, puts3Command, { expiresIn: 3600 })
 
-    return uploadUrl;
+    return uploadUrl
   }
 
   static createS3FileUrl(fileName: string): string {
-    return `${process.env.CLOUD_FLARE_PUBLIC_BUCKET_URL}/work-images/${fileName}`;
+    return `${process.env.CLOUD_FLARE_PUBLIC_BUCKET_URL}/work-images/${fileName}`
   }
 
   static createS3AvatarUrl(fileName: string): string {
-    return `${process.env.CLOUD_FLARE_PUBLIC_BUCKET_URL}/user-avatars-images/${fileName}`;
+    return `${process.env.CLOUD_FLARE_PUBLIC_BUCKET_URL}/user-avatars-images/${fileName}`
   }
 
   async uploadAvatarImage({ fileName, fileData }: FiletoUpload): Promise<FileUploadResponse> {
-    await this.createFolderIfNotExists('user-avatars-images');
+    await this.createFolderIfNotExists('user-avatars-images')
 
     const parsedImage = await this.imageTransformer.compressAndTransformImageToWebp({
       fileData,
       fileName,
-    });
+    })
 
     await this.s3Client.send(
       new PutObjectCommand({
@@ -68,12 +69,12 @@ export class CloudFlareR2StorageAdapter implements StorageProvider {
         Key: `user-avatars-images/${parsedImage.fileName}.${parsedImage.fileMimeType}`,
         ContentType: `image/${parsedImage.fileMimeType}`,
       }),
-    );
+    )
 
     return {
       fileType: parsedImage.fileMimeType,
       fileName: parsedImage.fileName,
-    };
+    }
   }
 
   private async createFolderIfNotExists(folderPrefix: string) {
@@ -81,37 +82,37 @@ export class CloudFlareR2StorageAdapter implements StorageProvider {
       Bucket: this.awsBucket,
       Prefix: folderPrefix,
       MaxKeys: 1,
-    });
+    })
 
-    const results = await this.s3Client.send(listObjectsCommand);
+    const results = await this.s3Client.send(listObjectsCommand)
 
-    const existsFolder = results.Contents?.length > 0;
+    const existsFolder = results.Contents?.length > 0
 
     if (!existsFolder) {
-      await this.createS3Folder(folderPrefix);
+      await this.createS3Folder(folderPrefix)
     }
   }
 
   private async createS3Folder(folderPrefix: string) {
     if (!folderPrefix.endsWith('/')) {
-      folderPrefix += '/';
+      folderPrefix += '/'
     }
 
     const createFolderCommand = new PutObjectCommand({
       Bucket: this.awsBucket,
       Key: folderPrefix,
-    });
+    })
 
-    await this.s3Client.send(createFolderCommand);
+    await this.s3Client.send(createFolderCommand)
   }
 
   async uploadWorkImage({ fileName, fileData }: FiletoUpload): Promise<FileUploadResponse> {
-    await this.createFolderIfNotExists('work-images');
+    await this.createFolderIfNotExists('work-images')
 
     const parsedImage = await this.imageTransformer.compressAndTransformImageToWebp({
       fileData,
       fileName,
-    });
+    })
 
     await this.s3Client.send(
       new PutObjectCommand({
@@ -120,25 +121,25 @@ export class CloudFlareR2StorageAdapter implements StorageProvider {
         Key: `work-images/${parsedImage.fileName}.${parsedImage.fileMimeType}`,
         ContentType: `image/${parsedImage.fileMimeType}`,
       }),
-    );
+    )
 
     return {
       fileType: parsedImage.fileMimeType,
       fileName: parsedImage.fileName,
-    };
+    }
   }
 
   async uploadWorkImageWithUrl({ fileName, fileData }: FiletoUploadWithUrl): Promise<FileUploadResponse> {
-    await this.createFolderIfNotExists('work-images');
+    await this.createFolderIfNotExists('work-images')
 
-    const response = await fetch(fileData);
+    const response = await fetch(fileData)
 
-    const buffer = await response.arrayBuffer();
+    const buffer = await response.arrayBuffer()
 
     const parsedImage = await this.imageTransformer.compressAndTransformImageToWebp({
       fileData: buffer,
       fileName,
-    });
+    })
 
     await this.s3Client.send(
       new PutObjectCommand({
@@ -147,10 +148,10 @@ export class CloudFlareR2StorageAdapter implements StorageProvider {
         Key: `work-images/${parsedImage.fileName}.${parsedImage.fileMimeType}`,
         ContentType: `image/${parsedImage.fileMimeType}`,
       }),
-    );
+    )
     return {
       fileType: parsedImage.fileMimeType,
       fileName: parsedImage.fileName,
-    };
+    }
   }
 }
